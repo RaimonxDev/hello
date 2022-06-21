2639b03119b146b3c343baf0892d497b9b8c8e6d
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2022-06-21 10:50:04 -0400

    Initial commit

---
+++ greet/greet.go
@@ -0,0 +1,10 @@
+package greet
+
+var emoji = "💚"
+
+func English() string {
+	return "Hi" + emoji
+}
+func Italian() string {
+	return "Ciao" + emoji
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	saludo := greet.English()
+
+	fmt.Println(saludo)
+
+}
