d198ea6f15accf4cf08c8411ff607a3b4758829d
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2022-06-21 11:08:12 -0400

    modulo de terceros

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// github.com/RaimonxDev/hello es la ruta de importación del paquete greet
+	"github.com/RaimonxDev/hello/greet"
+	"rsc.io/quote"
+)
 
 func main() {
 
 	saludo := greet.English()
 
 	fmt.Println(saludo)
+	fmt.Println(quote.Hello())
 
 }
